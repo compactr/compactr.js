416ef155f580335955bf770f51995876ac5bb7bf
fed135 <fredericcharette@gmail.com>
2016-08-16 23:40:42 -0400

    fixed test badge

--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,5 @@
+const Compactr = require('../');
+
 let User = { user_id: 'number', user_name: 'string', x: 'number', y: { type: 'number', defaultsTo: 0}, z: 'number', alive: 'boolean'};
 let packed = Compactr.encode(User, { user_id: 255, user_name: 'steve', x: 32, y: 2000, z: 0, alive: false});
 console.log(packed);
